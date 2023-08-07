ab19e3563cebffe47169a38a769c10145ac4270a
Jean <jeandehorta@outlook.com>
2023-08-06 19:50:55 -0500

    addEventListener en js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const txtOp2 = document.getElementById("op2");
 const btnCalcular = document.getElementById("calcular");
 const pResultado = document.getElementById("resultado");
 
+btnCalcular.addEventListener('click', calcular);
 function calcular(){
     console.log("apretaste bien")
 }
