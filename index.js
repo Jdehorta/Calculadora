dfb26899bdb1a2ddd85dfe17dde11e29a8aa7279
Jean <jeandehorta@outlook.com>
2023-08-06 21:31:21 -0500

    Calculadora final manejo de DOM

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 //por medio de id conectamos los elementos
 
 const txtOp1 = document.getElementById("op1");
-const txtOperador = document.getElementById("operador");
+const txtOperacion = document.getElementById("operacion");
 const txtOp2 = document.getElementById("op2");
 const btnCalcular = document.getElementById("calcular");
 const pResultado = document.getElementById("resultado");
 
 btnCalcular.addEventListener('click', calcular);
 function calcular(){
-    console.log(txtOperador.value)
-    const operador = txtOperador.value
-    if(operador == "+" || operador == "-" || operador == "*" || operador == "/"){
-        console.log("Calculo posible")
+     const operacion = txtOperacion.value
+     const op1 = parseFloat(txtOp1.value)
+     const op2 = parseFloat(txtOp2.value)
+
+    if((operacion == "+" || operacion == "-" || operacion == "*" ||
+     operacion == "/") && !isNaN(op1) && !isNaN(op2) ){
+        let resultado;
+        switch(operacion){
+            case "+":
+                resultado = op1 + op2;
+                break;
+            case "-":
+                resultado = op1 - op2;
+                break;
+            case "*":
+                resultado = op1 * op2;
+                break;
+            case "/":
+                resultado = op1 / op2;
+                break;
+                
+
+       }
+       pResultado.style = "color:black"
+        pResultado.innerText = "El resultado es = " + resultado
     }else{
-        console.log("Calculo imposible")
+        pResultado.innerText =  "Calculo imposible, debe utilizar los operadores +,-,/,*, no puede estar vacio"
+        pResultado.style = "color:red"
     }
+
 }
